bfc3894a2de8c968b8732cc360a70949dbdd64ab
jaoincode <ojoaogugel@gmail.com>
2023-01-26 10:46:21 -0300

    theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "my-gray-100": "#434343",
+      "my-grah-200": "#373737",
+      "my-black-100": "181818",
+      "my-black-200": "151515",
+      "my-white-100": "#ffffff",
+      "my-white-200": "#EBEBEB",
+      "my-blue-100": "#0099FF",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
