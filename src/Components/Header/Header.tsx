d87cc5d65ca62176d0ef0b595e7f17631042246d
jaoincode <ojoaogugel@gmail.com>
2023-01-29 13:32:44 -0300

    starting header & home page

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import FileIcon from "../../assets/file.svg";
+import HamburguerIcon from "../../assets/hamburguer.svg";
+import SaveIcon from "../../assets/save.svg";
+import TrashIcon from "../../assets/trash.svg";
+
+function Header() {
+  const isOnFile = true;
+
+  return (
+    <header className="w-full flex p-2 md:p-4 items-center h-14 bg-mygray-100 fixed justify-between">
+      <div className="flex items-center">
+        <button aria-label="Menu" className="w-7 md:w-9 ">
+          <img src={HamburguerIcon} />
+        </button>
+        <h1 className="font-bold text-mywhite-200 mx-2 md:text-lg">marknow</h1>
+        {isOnFile && (
+          <div className="flex items-center md:mx-7">
+            <img src={FileIcon} alt="File icon" className="w-7 md:w-9" />
+            <span className="font-medium mx-1 text-mywhite-200 md:text-lg">
+              filename.md
+            </span>
+          </div>
+        )}
+      </div>
+      {isOnFile && (
+        <div className="flex items-center gap-2">
+          <button
+            aria-label="Delete file"
+            className="w-7 md:w-9 bg-red-600 p-1 rounded-full"
+          >
+            <img src={TrashIcon} />
+          </button>
+          <button
+            aria-label="Save file"
+            className="w-7 bg-myblue-100 p-1 rounded-full md:flex md:w-36 md:h-9 items-center"
+          >
+            <img src={SaveIcon} className="md:w-8 md:h-8" />
+            <p className="hidden md:inline-block font-medium text-mywhite-100">
+              Save Changes
+            </p>
+          </button>
+        </div>
+      )}
+    </header>
+  );
+}
+
+export default Header;
