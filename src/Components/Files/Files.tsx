b17186ff74415860fbfb679fa530b44ef0cb2049
jaoincode <ojoaogugel@gmail.com>
2023-01-29 14:09:27 -0300

    adding wave img on menu

--- src/Components/Files/Files.tsx
+++ src/Components/Files/Files.tsx
@@ -4,7 +4,7 @@ import File from "../File";
 
 function Files() {
   return (
-    <div className="pt-20 px-3 gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pb-5">
+    <div className="pt-20 px-3 gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pb-5 z-20">
       {files.map((file) => (
         <File name={file.name} />
       ))}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,14 @@
 import Files from "../../Components/Files";
 import Header from "../../Components/Header";
 
+import WaveImg from "../../assets/wave.svg";
+
 function Home() {
   return (
     <div>
       <Header />
       <Files />
+      <img src={WaveImg} className="min-w-screen fixed bottom-0 z-10" />
     </div>
   );
 }
